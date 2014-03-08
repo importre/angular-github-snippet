0d5878d32e38cc71170f9be005a6d55f21e73fbb
importre <importre@importre-ui-MacBook-Air.local>
2014-03-09 04:33:18 +0900

    example->demo

--- javascripts/app.js
+++ javascripts/app.js
@@ -2,7 +2,7 @@ var app = angular.module("app", ["ghsnip"]);
 
 app.controller("appCtrl", ["$scope",
   function ($scope) {
-    $scope.title = "angular-github-snippet example";
+    $scope.title = "angular-github-snippet demo";
     $scope.subTitle1 = "All Lines";
     $scope.subTitle2 = "5~11, 18, 19, 20, 24~27";
     $scope.examSrc = "https://api.github.com/repos/importre/chromeadb/contents/src/scripts/chromeadb.js?ref=655acdf29086f6b3e933b17404597674eaf9f9a0";
