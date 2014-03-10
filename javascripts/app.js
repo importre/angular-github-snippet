2ab8335ce6c65217a511df7aa38ff8aeabdf9bca
importre <importre@icloud.com>
2014-03-11 00:06:35 +0900

    Update

--- javascripts/app.js
+++ javascripts/app.js
@@ -2,7 +2,7 @@ var app = angular.module("app", ["ghsnip"]);
 
 app.controller("appCtrl", ["$scope",
   function ($scope) {
-    $scope.code = "https://github.com/importre/angular-github-snippet/blob/1352919f8baef25e2a771239fa21d281b91e8279/index.html";
-    $scope.app = "https://github.com/importre/angular-github-snippet/blob/gh-pages/javascripts/app.js";
+    $scope.code = "https://github.com/importre/angular-github-snippet/blob/381d519c8baadf5273f24d50e7182c85fafbc970/index.html";
+    $scope.app = "https://github.com/importre/angular-github-snippet/blob/381d519c8baadf5273f24d50e7182c85fafbc970/javascripts/app.js";
   }
 ]);
