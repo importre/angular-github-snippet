db0e426a19023363e1ee83fe583486a0c75cd91f
importre <importre@icloud.com>
2014-03-10 20:03:38 +0900

    Update

--- javascripts/app.js
+++ javascripts/app.js
@@ -2,10 +2,6 @@ var app = angular.module("app", ["ghsnip"]);
 
 app.controller("appCtrl", ["$scope",
   function ($scope) {
-    $scope.title = "angular-github-snippet demo";
-    $scope.subTitle1 = "All Lines";
-    $scope.subTitle2 = "5~11, 18, 19, 20, 24~27";
-    $scope.examSrc = "https://api.github.com/repos/importre/chromeadb/contents/src/scripts/chromeadb.js?ref=655acdf29086f6b3e933b17404597674eaf9f9a0";
-    $scope.lines = "5:11,18,19,20,24:27";
+    $scope.code = "https://github.com/importre/angular-github-snippet/blob/1352919f8baef25e2a771239fa21d281b91e8279/index.html";
   }
 ]);
